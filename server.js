4acc880faa1ee74882a0b49ddc94e9da5b4294be
Felipe Gadea Castro <felipegadeacastro@MacBook-Pro-de-Felipe.local>
2020-11-10 20:59:14 -0600

    feature: create server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app');
+
+const port = 5000; //precess.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('Server is running in port: ', port);
