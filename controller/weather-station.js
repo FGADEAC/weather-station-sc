f407b55dacc44a1b5cd0488eda3e79d5c9ba3b67
Felipe Gadea Castro <felipegadeacastro@MacBook-Pro-de-Felipe.local>
2020-11-10 21:54:02 -0600

    add post route (example)

--- controller/weather-station.js
+++ controller/weather-station.js
@@ -8,4 +8,12 @@ weatherStation.get = (req, res) => {
     })
 };
 
+weatherStation.post = (req, res) => {
+    console.log(req.body)
+    const sensor1 = req.body.sensor1;
+    return res.status(200).json({
+        message: `Hola mundo: ${sensor1}`
+    })
+};
+
 module.exports = weatherStation;
--- route/weather-station.js
+++ route/weather-station.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controller/weather-station');
 
 router.get('/', controller.get);
+router.post('/', controller.post)
 
 module.exports = router;
